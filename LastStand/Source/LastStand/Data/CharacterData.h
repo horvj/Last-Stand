bf129d0a1ec2044eb4fad5c140a5dbea58c4e925
horvj <143136651+horvj@users.noreply.github.com>
2025-10-30 15:12:38 -0400

    create character info data asset and add GAS plugin to project

---
+++ LastStand/Source/LastStand/Data/CharacterData.h
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "CharacterData.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LASTSTAND_API UCharacterData : public UDataAsset
+{
+	GENERATED_BODY()
+
+public:
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Character")
+    FString CharacterName;
+
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Character")
+    class USkeletalMesh* SkeletalMesh;
+
+    /*UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Abilities")
+    TSubclassOf<class UGameplayAbility> SpecialAbility;*/
+
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Stats")
+    float Health = 100.0f;
+
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Stats")
+    float AttackPower = 1.0f;
+
+	
+};
